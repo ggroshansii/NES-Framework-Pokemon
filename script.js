3c51aef9a2da2e7455cbb3c4027e25fe3af0581a
ggroshansii <garthgroshansii@gmail.com>
2021-02-15 15:36:22 -0500

    added more JS that changes innerHTML based on click events

--- script.js
+++ script.js
@@ -6,18 +6,30 @@ console.log(pokemonz);
 
 for (let pokemon of pokemonz){
     pokemon.addEventListener("click", function(){
-        if (pokemon.id == "bulbasaur" && pokemon.innerHTML = ""){
+        const bulb = document.querySelector("#bulbasaur");
+        const char = document.querySelector("#charmander");
+        const squirt = document.querySelector("#squirtle");
+
+        if (pokemon.id == "bulbasaur" && pokemon.innerHTML == '<i class="nes-pokeball"></i>'){
             console.log("bulbasaur")
             pokemon.innerHTML = "";
-            pokemon.innerHTML = '<i class="nes-bulbasaur"></i>';
-        } else if (pokemon.id == "charmander"){
+            pokemon.innerHTML = '<a href= "https://pokemon.fandom.com/wiki/Bulbasaur"><i class="nes-bulbasaur"></i></a>';
+            char.innerHTML = '<i class="nes-pokeball"></i>';
+            squirt.innerHTML = '<i class="nes-pokeball"></i>';
+       
+       
+    } else if (pokemon.id == "charmander" && pokemon.innerHTML == '<i class="nes-pokeball"></i>'){
             console.log("charmander")
             pokemon.innerHTML = "";
-            pokemon.innerHTML = '<i class="nes-charmander"></i>';
-        } else if (pokemon.id == "squirtle"){
+            pokemon.innerHTML = '<a href= "https://pokemon.fandom.com/wiki/Charmander"><i class="nes-charmander"></i></a>';
+            bulb.innerHTML = '<i class="nes-pokeball"></i>'
+            squirt.innerHTML = '<i class="nes-pokeball"></i>';
+        } else if (pokemon.id == "squirtle" && pokemon.innerHTML == '<i class="nes-pokeball"></i>'){
             console.log("squrirtle")
             pokemon.innerHTML = "";
-            pokemon.innerHTML = '<i class="nes-squirtle"></i>';
+            pokemon.innerHTML = '<a href= "https://pokemon.fandom.com/wiki/Squirtle"><i class="nes-squirtle"></i></a>';
+            char.innerHTML = '<i class="nes-pokeball"></i>';
+            bulb.innerHTML = '<i class="nes-pokeball"></i>'
         }
         
     });
